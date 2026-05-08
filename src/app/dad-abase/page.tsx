ba1c52cb297b939f156391cea737bae1c94f966b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2026-05-08 12:35:33 -0400

    initial setup of images, font, and basic CSS

---
+++ src/app/dad-abase/page.tsx
@@ -0,0 +1,9 @@
+export default function DadAbase() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1 style={{ fontFamily: "var(--font-fredericka)" }}>
+        Dad Abase
+      </h1>
+    </main>
+  );
+}
