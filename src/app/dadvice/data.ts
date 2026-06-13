24a99fd8f210f2cb6439bd4f8716051db7c6bb9d
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2026-06-13 17:07:56 -0400

    Update data.ts
    added credits to two points of data in dadvice data

--- src/app/dadvice/data.ts
+++ src/app/dadvice/data.ts
@@ -1,13 +1,13 @@
 export const dadAdvice: [string, string?][] = [
   ["Practice will never make perfect. Practice makes possibilities."],
   [
-    "Character is everything. Have a good heart. Life will happen but as long as you stay true and live with passion you'll succeed.",
+    "Character is everything. Have a good heart. Life will happen but as long as you stay true and live with passion you'll succeed.", "Rich"
   ],
   [
     "The most precious thing you have to offer in this world is your time. Look how much of it you give away to different activities and people, and make sure they are worth it.",
-    "Dicky Kitchen Jr",
+    "Dicky",
   ],
   [
-    "Never be too prideful to apologize to your kids. If you want your kids to have healthy relationships, show them what one looks like with your spouse.",
+    "Never be too prideful to apologize to your kids. If you want your kids to have healthy relationships, show them what one looks like with your spouse.", "Gavin",
   ],
 ];
